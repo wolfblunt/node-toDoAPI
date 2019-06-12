f9a9c00b33ae6669eab6c3f152659842d5cadc82
wolfblunt <amankhandelwaljuly@gmail.com>
2019-06-12 14:11:03 +0530

    Add x-AuthToken in login

--- server/server.js
+++ server/server.js
@@ -129,7 +129,9 @@ app.post('/users/login', (req,res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
   User.findByCredentials(body.email, body.password).then((user) => {
-    res.send(user);
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
   }).catch((e) => {
     res.status(400).send();
   });
