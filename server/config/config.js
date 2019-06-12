98b85c5a53996f07c7636c5a6e55662d05d683f6
wolfblunt <amankhandelwaljuly@gmail.com>
2019-06-12 13:40:50 +0530

    Security and Authentication Part done

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,14 @@
 var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/NodeMongoApp';
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/NodeMongoApp';
+// }
